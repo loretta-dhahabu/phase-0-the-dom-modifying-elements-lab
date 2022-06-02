d3b650de7ae390f8202f3f4d75651d91d59c4ba6
loretta-dhahabu <loreydjefwa@gmail.com>
2022-06-02 20:35:50 +0300

    commit tree c18b4b48f50d4cc9b51420428f1d00f83d4556f6

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const element = document.querySelector( 'main' );
+element.remove();
+const newHeader = document.createElement( 'h1' );
+newHeader.id = "victory";
+newHeader.innerHTML = "Dhahabu is the champion";
